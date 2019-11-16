be021efe52138187c51672789e174b2749b14f1c
Angel Contreras <angelctmex@gmail.com>
2019-11-15 21:30:41 -0600

    Implementing fahrenheit to celsius conversion

--- WorldTrotter/ ConversionViewController.swift
+++ WorldTrotter/ ConversionViewController.swift
@@ -11,12 +11,57 @@ import UIKit
 class ConversionViewController: UIViewController {
     
     
-    @IBOutlet weak var celsiusLabel: UILabel!
+    @IBOutlet var celsiusLabel: UILabel!
+    @IBOutlet weak var textField: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        updateCelsiusLabel()
+    }
+    
+    var fahrenheitValue: Measurement<UnitTemperature>? {
+        didSet {
+            updateCelsiusLabel()
+        }
+    }
     
    
     @IBAction func fahrenheitFieldEditingChanged(_ textField: UITextField) {
-        celsiusLabel.text = textField.text
+        
+        if let text = textField.text, let value = Double(text) {
+            fahrenheitValue = Measurement(value: value, unit: .fahrenheit)
+        } else {
+            fahrenheitValue = nil
+        }
     }
     
+   
+    @IBAction func dismissKeyboard(_ sender: UITapGestureRecognizer) {
+        textField.resignFirstResponder()
+    }
+    
+    var celsiusValue: Measurement<UnitTemperature>? {
+        if let fahrenheitValue = fahrenheitValue {
+            return fahrenheitValue.converted(to: .celsius)
+        } else {
+            return nil
+        }
+    }
+    
+    func updateCelsiusLabel() {
+        if let celsiusValue = celsiusValue {
+            celsiusLabel.text = numberFormatter.string(from: NSNumber(value: celsiusValue.value))
+        } else {
+            celsiusLabel.text = "???"
+        }
+    }
+    
+    let numberFormatter: NumberFormatter = {
+        let nf = NumberFormatter()
+        nf.numberStyle = .decimal
+        nf.minimumFractionDigits = 0
+        nf.maximumFractionDigits = 1
+        return nf
+    }()
     
 }
